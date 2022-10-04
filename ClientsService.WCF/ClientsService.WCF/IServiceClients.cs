f4af07fb9a4983c5deab3151638b28e770ed8f88
yevhensd <yevhen.sd@gmail.com>
2022-10-04 19:20:05 +0300

    ending wcf

--- ClientsService.WCF/ClientsService.WCF/IServiceClients.cs
+++ ClientsService.WCF/ClientsService.WCF/IServiceClients.cs
@@ -17,54 +17,6 @@ namespace ClientsService.WCF
         RequestFormat = WebMessageFormat.Json,
         ResponseFormat = WebMessageFormat.Json,
         BodyStyle = WebMessageBodyStyle.Bare)]
-        string GetClientsJson(string city);
+        int GetClientsJson(string city);
     }
-
-    [DataContract]
-    public class Timetable
-    {
-        [DataMember]
-        public int id { get; set; }
-
-        [DataMember]
-        public DateTime arrivaltime { get; set; }
-
-        [DataMember]
-        public Int16 busnumber { get; set; }
-
-        [DataMember]
-        public string busstation { get; set; }
-    }
-
-    [DataContract]
-    public class Client
-    {
-        [DataMember]
-        public Guid Id { get; set; }
-
-        [DataMember]
-        public string Name { get; set; }
-
-        [DataMember]
-        public string Gender { get; set; }
-
-        [DataMember]
-        public string Email { get; set; }
-
-        [DataMember]
-        public string Phone { get; set; }
-
-        [DataMember]
-        public string City { get; set; }
-
-        [DataMember]
-        public DateTime Birthday { get; set; }
-
-        [DataMember]
-        public DateTime Created { get; set; }
-
-        [DataMember]
-        public string Comment { get; set; }
-    }
-
 }
--- ClientsService.WCF/ClientsService.WCF/ServiceClients.svc.cs
+++ ClientsService.WCF/ClientsService.WCF/ServiceClients.svc.cs
@@ -14,15 +14,14 @@ namespace ClientsService.WCF
     {
         public string ConnectionString = "Data Source=localhost;Initial Catalog=ClientsBase;Persist Security Info=True;User ID=sa;Password=01234567;";
 
-        public string GetClientsJson(string city)
+        public int GetClientsJson(string city)
         {
             return GetClients(city);
         }
 
-        private string GetClients(string city)
+        private int GetClients(string city)
         {
-            List<Client> Clients = new List<Client>();
-            int i = 0;
+            int count = 0;
 
             using (DataSet ds = new DataSet())
             {
@@ -35,12 +34,12 @@ namespace ClientsService.WCF
                         using (SqlDataAdapter sqlDa = new SqlDataAdapter(sqlStr, sqlCon))
                         {
                             sqlDa.Fill(ds);
-                            i = ds.Tables[0].Rows.Count;
+                            count = ds.Tables[0].Rows.Count;
                         }
                     }
                     catch
                     {
-                        return "-1";
+                        return -1;
                     }
                     finally
                     {
@@ -48,38 +47,9 @@ namespace ClientsService.WCF
                     }
                 }                
 
-                using (DataTable dt = ds.Tables[0])
-                {
-                    foreach (DataRow dr in dt.Rows)
-                    {
-                        Clients.Add(new Client()
-                        {
-                            Id = Guid.Parse(dr["id"].ToString()),
-                            Name = dr["name"].ToString(),
-                            Gender = dr["gender"].ToString(),
-                            Email = dr["email"].ToString(),
-                            Phone = dr["phone"].ToString(),
-                            City = dr["city"].ToString(),
-                            Birthday = DateTime.Parse(dr["birthday"].ToString()),
-                            Created = DateTime.Parse(dr["created"].ToString()),
-                            Comment = dr["comment"].ToString()
-                        });
-                    }
-                }
             }
 
-          List<Timetable> Schedule = new List<Timetable>
-          {
-            new Timetable
-            {
-                id=1, arrivaltime=DateTime.Parse("12:05:00"), busnumber=5, busstation = Clients.Count.ToString()
-            },
-            new Timetable
-            {
-                id=2, arrivaltime =DateTime.Parse("12:10:00"), busnumber=5, busstation ="Детский мир"
-            }
-          };
-            return i.ToString();
+            return count;
         }
     }
 }
